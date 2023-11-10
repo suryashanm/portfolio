67915acd3077b4d8b70aca71e0b5e1ab9608259e
suryashanm <shanmsurya@gmail.com>
2023-11-10 13:00:33 +0530

    header test

--- workplace/grunt/dist/script.js
+++ workplace/grunt/dist/script.js
@@ -1,4 +1,4 @@
-/*Processed by SURYA on 10/11/2023@12:26:51*/
+/*Processed by SURYA on 10/11/2023@12:59:22*/
 // Event listener for toggling hamburger menu, menu and CV file display on click.
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const menu = document.querySelector('.menu');
