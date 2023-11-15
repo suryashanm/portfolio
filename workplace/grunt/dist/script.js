5173152a1c05296ceed1a05f104a666cbd107a3e
suryashanm <shanmsurya@gmail.com>
2023-11-15 12:12:24 +0530

    about

--- workplace/grunt/dist/script.js
+++ workplace/grunt/dist/script.js
@@ -1,4 +1,4 @@
-/*Processed by SURYA on 12/11/2023@18:7:14*/
+/*Processed by SURYA on 15/11/2023@9:34:58*/
 // Event listener for toggling hamburger menu, menu and CV file display on click.
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const menu = document.querySelector('.menu');
